82f785fbbb66b1afe0eea87c693104dec10ab871
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-17 13:05:44 +0330

    make swagger page

--- RentifyBackend/users/views.py
+++ RentifyBackend/users/views.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +7,8 @@ from .serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterView(APIView):
+
+    @swagger_auto_schema(request_body=RegisterSerializer)
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -16,6 +19,8 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+
+    @swagger_auto_schema(request_body=LoginSerializer)
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
