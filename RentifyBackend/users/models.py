3b53b441aee66e0b3fcaf0261755274679e07ebb
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-17 11:41:13 +0330

    add __str__ to user model

--- RentifyBackend/users/models.py
+++ RentifyBackend/users/models.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=11, blank=False, null=False)
-    national_code = models.CharField(max_length=10, blank=False, null=False)
+    phone_number = models.CharField(max_length=11, unique=True)
+    national_code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
