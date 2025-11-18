b3f14aa0a44323babd7506a350a42d87c62deeea
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-18 10:36:17 +0330

    create vehicle view

--- RentifyBackend/vehicles/views.py
+++ RentifyBackend/vehicles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Vehicle
+from .serializer import VehicleSerializer
 
-# Create your views here.
+class VehicleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Vehicle.objects.all().order_by('-created_at')
+    serializer_class = VehicleSerializer
+    permission_classes = [permissions.AllowAny]
