c713a28d339c70098caab064819118cf3dce3cd9
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-18 10:59:32 +0330

    change model directory

--- RentifyBackend/vehicles/serializer.py
+++ RentifyBackend/vehicles/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Vehicle, VehicleImages
+from .models import Vehicle, VehicleImages
 
 class VehicleImagesSerializer(serializers.ModelSerializer):
     class Meta:
