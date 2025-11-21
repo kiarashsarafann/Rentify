d9ad41eb217d307ac4e9cd1ebd4d73645fdade05
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-22 00:42:53 +0330

    add jalali_date to settings

--- RentifyBackend/RentifyBackend/settings.py
+++ RentifyBackend/RentifyBackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'jalali_date',
     'drf_yasg',
     'users',
     'vehicles',
@@ -123,7 +124,6 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
