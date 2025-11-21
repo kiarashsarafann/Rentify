57f7b8c38bd68cecc06219b245d95ed36aeb760e
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-21 21:30:04 +0330

    set media urls

--- RentifyBackend/RentifyBackend/urls.py
+++ RentifyBackend/RentifyBackend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
