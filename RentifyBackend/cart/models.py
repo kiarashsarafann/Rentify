d9cc86c59ca097b0355e9169d15db2cf1fd8a4ef
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-22 00:45:46 +0330

    create cartitem model

--- RentifyBackend/cart/models.py
+++ RentifyBackend/cart/models.py
@@ -10,3 +10,24 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="vehicle")
+
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+    total_price = models.DecimalField(max_digits=10, decimal_places=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def calculate_price(self):
+        duration = self.end_datetime - self.start_datetime
+        hours = duration.total_seconds() / 3600
+
+        if hours >= 24:
+            days = hours // 24
+            return days * self.vehicle.price_per_day
+
+        return hours * self.vehicle.price_per_hour
+
