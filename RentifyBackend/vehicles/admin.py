057b66d09c2e8129500310e61cff66b2c36ddaec
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-23 22:06:34 +0330

    create vehicle admin page

--- RentifyBackend/vehicles/admin.py
+++ RentifyBackend/vehicles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from vehicles.models import Vehicle, VehicleImages
+
+admin.site.register(VehicleImages)
+
+class VehicleImagesInline(admin.TabularInline):
+    model = VehicleImages
+    extra = 1
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "brand", "model", "year","price_per_hour", "price_per_day", "is_available")
+    inlines = [VehicleImagesInline]
+
+
