c92688eaa28010fae988c5d7d383ec956c930dc0
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-23 16:08:23 +0330

    create cart detail view

--- RentifyBackend/cart/views.py
+++ RentifyBackend/cart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+from .models import Cart, CartItem
+from .serializers import (
+    CartSerializer,
+    AddCartItemSerializer,
+    RemoveCartItemSerializer,
+)
+
+
+class CartDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+
+
