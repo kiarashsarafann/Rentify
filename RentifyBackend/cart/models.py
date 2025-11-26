3eab321f109715452883fd949417a7187bb1d7e8
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-11-27 03:13:16 +0330

    change caculate_price function

--- RentifyBackend/cart/models.py
+++ RentifyBackend/cart/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from vehicles.models import Vehicle
 from django_jalali.db import models as jmodels
+from decimal import Decimal
 
 User = get_user_model()
 
@@ -12,7 +13,7 @@ class Cart(models.Model):
 
     @property
     def calculate_total(self):
-        return sum(item.total_price for item in self.items.all())
+        return sum((item.total_price or 0) for item in self.items.all())
 
     def __str__(self):
         return f"Cart of {self.user.phone_number}"
@@ -26,15 +27,25 @@ class CartItem(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def calculate_price(self):
-        duration = self.end_time - self.start_time
-        hours = duration.total_seconds() / 3600
+    @property
+    def calculate_item(self):
 
-        if hours >= 24:
-            days = hours // 24
-            return days * self.vehicle.price_per_day
+        duration = self.end_time - self.start_time
+        hours = Decimal(duration.total_seconds()) / Decimal(3600)
+        days = hours // 24
+
+
+        if days >= 1:
+            hours = hours - (days * 24)
+            print(hours)
+            print(days)
+            return (days * self.vehicle.price_per_day) + (hours * self.vehicle.price_per_hour)
 
         return hours * self.vehicle.price_per_hour
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.calculate_item
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.vehicle.name} — {self.start_time} to {self.end_time}"
