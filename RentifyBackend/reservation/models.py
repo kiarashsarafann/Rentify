f4b86525526e3626aa14298f9c0a20ad41666010
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-12-01 13:00:32 +0330

    create reservation model

--- RentifyBackend/reservation/models.py
+++ RentifyBackend/reservation/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django_jalali.db import models as jmodels
+from vehicles.models import Vehicle
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ReservationStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    CONFIRMED = "confirmed", "Confirmed"
+    CANCELED = "canceled", "Canceled"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    start_time = jmodels.jDateTimeField()
+    end_time = jmodels.jDateTimeField()
+    status = models.CharField(
+        max_length=10,
+        choices=ReservationStatus.choices,
+        default=ReservationStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
