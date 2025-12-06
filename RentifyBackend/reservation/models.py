d0e2634d17e1271986c9911fe9c8bc8cbaa15613
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-12-06 14:56:20 +0330

    change reservation model

--- RentifyBackend/reservation/models.py
+++ RentifyBackend/reservation/models.py
@@ -1,10 +1,12 @@
-from django.db import models
+from decimal import Decimal
 
+from django.db import models
 from django_jalali.db import models as jmodels
-from vehicles.models import Vehicle
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 class ReservationStatus(models.TextChoices):
     PENDING = "pending", "Pending"
@@ -14,19 +16,37 @@ class ReservationStatus(models.TextChoices):
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey("vehicles.Vehicle", on_delete=models.CASCADE)
     start_time = jmodels.jDateTimeField()
     end_time = jmodels.jDateTimeField()
+
     status = models.CharField(
         max_length=10,
         choices=ReservationStatus.choices,
         default=ReservationStatus.PENDING
     )
+
     total_price = models.DecimalField(max_digits=10, decimal_places=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def calculate_price(self):
+        duration = self.end_time - self.start_time
+        hours = Decimal(duration.total_seconds()) / Decimal(3600)
+        days = hours // 24
+
+        if days >= 1:
+            hours = hours - (days * 24)
+            return (days * self.vehicle.price_per_day) + (hours * self.vehicle.price_per_hour)
+
+        return hours * self.vehicle.price_per_hour
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.calculate_price
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["-created_at"]
 
     def __str__(self):
-        return f"{self.vehicle.name} - {self.start_time} to {self.end_time}"
+        return f"{self.user.name} | {self.total_price} | {self.status}"
