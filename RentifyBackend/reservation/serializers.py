6f679b5e4dff02d5e26d25ad324c883d9855e09b
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-12-06 14:58:16 +0330

    create serializer for reservation

---
+++ RentifyBackend/reservation/serializers.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+from django_jalali.serializers.serializerfield import JDateTimeField
+
+from reservation.models import Reservation, ReservationStatus
+from vehicles.models import Vehicle
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    vehicle_id = serializers.IntegerField()
+    vehicle_name = serializers.CharField(source="vehicle.name", read_only=True)
+    user_id = serializers.IntegerField(source="user.id", read_only=True)
+    user_name = serializers.CharField(source="user.name", read_only=True)
+    start_time = JDateTimeField()
+    end_time = JDateTimeField()
+    status = serializers.CharField(source="status.name", read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Reservation
+        fields = [
+            "id",
+            "user_id",
+            "user_name",
+            "vehicle_id",
+            "vehicle_name",
+            "start_time",
+            "end_time",
+            "status",
+            "total_price",
+            "created_at",
+        ]
+
+    def get_total_price(self, obj):
+        return obj.calculate_price
+
+    def validate(self, data):
+        if data["end_time"] <= data["start_time"]:
+            raise serializers.ValidationError("زمان پایان باید بعد از شروع باشد")
+
+        try:
+            vehicle = Vehicle.objects.get(id=data["vehicle_id"])
+        except Vehicle.DoesNotExist:
+            raise serializers.ValidationError("وسیله پیدا نشد")
+        data["vehicle"] = vehicle
+
+        if Reservation.objects.filter(vehicle=vehicle,
+                                      status__in=[ReservationStatus.PENDING, ReservationStatus.CONFIRMED],
+                                      start_time__lt=data["end_time"],
+                                      end_time__gt=data["start_time"],
+                                      ).exists():
+            raise serializers.ValidationError("این بازه زمانی با یک آیتم دیگه تداخل دارد!")
+
+        return data
+
+    def update(self, instance, validated_data):
+        instance.status = validated_data["status"]
+        instance.save()
+        return instance
--- RentifyBackend/reservation/views.py
+++ RentifyBackend/reservation/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Reservation
+from .serializers import ReservationSerializer
+
+class ReservationViewSet(
+    mixins.ListModelMixin,      # GET list
+    mixins.RetrieveModelMixin,  # GET detail
+    mixins.CreateModelMixin,    # POST
+    mixins.UpdateModelMixin,    # PATCH/PUT
+    viewsets.GenericViewSet
+):
+    serializer_class = ReservationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
