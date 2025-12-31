301befda0adfbb44583b626c0293543ae5f3de6f
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2025-12-31 04:29:49 +0330

    add auth to swagger page

--- RentifyBackend/RentifyBackend/urls.py
+++ RentifyBackend/RentifyBackend/urls.py
@@ -5,6 +5,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAdminUser
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -12,8 +14,9 @@ schema_view = get_schema_view(
         default_version='v1',
         description="API documentation for Rentify",
     ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+    public=False,
+    authentication_classes=[SessionAuthentication],
+    permission_classes=[IsAdminUser],
 )
 
 urlpatterns = [
@@ -21,10 +24,9 @@ urlpatterns = [
 
     path('api/users/', include('users.urls')),
     path('api/vehicles/', include('vehicles.urls')),
-    # path('api/carts/', include('cart.urls')),
     path('api/reservations/', include('reservation.urls')),
 
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
 
