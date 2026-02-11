c467021c63230e325e1680032ef42a6be5546e7d
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2026-02-12 01:02:08 +0330

    make admin for reservation

--- RentifyBackend/reservation/admin.py
+++ RentifyBackend/reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from reservation.models import Reservation
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vehicle', 'start_time', 'end_time', 'total_price', 'status', 'created_at')
