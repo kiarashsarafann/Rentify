23c9dfe267a7e929cad8c13d429b0b9cc60113b4
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2026-02-12 01:04:29 +0330

    add verbose name to model fields

--- RentifyBackend/vehicles/models.py
+++ RentifyBackend/vehicles/models.py
@@ -8,24 +8,33 @@ class VehicleType(models.TextChoices):
 
 
 class Vehicle(models.Model):
-    name = models.CharField(max_length=64)
-    type = models.CharField(max_length=10, choices=VehicleType.choices, default=VehicleType.CAR)
-    brand = models.CharField(max_length=32)
-    model = models.CharField(max_length=32)
-    year = models.IntegerField(null=True, blank=True)
-    color = models.CharField(max_length=32)
-    price_per_hour = models.DecimalField(max_digits=8,decimal_places=0, null=True, blank=True)
-    price_per_day = models.DecimalField(max_digits=8, decimal_places=0, null=True, blank=True)
-    is_available = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=64, verbose_name='نام')
+    type = models.CharField(max_length=10, choices=VehicleType.choices, default=VehicleType.CAR, verbose_name='نوع')
+    brand = models.CharField(max_length=32, verbose_name='برند')
+    model = models.CharField(max_length=32, verbose_name='مدل')
+    year = models.IntegerField(null=True, blank=True, verbose_name='سال تولید')
+    color = models.CharField(max_length=32, verbose_name='رنگ')
+    price_per_hour = models.DecimalField(max_digits=8,decimal_places=0, null=True, blank=True, verbose_name='قیمت در ساعت')
+    price_per_day = models.DecimalField(max_digits=8, decimal_places=0, null=True, blank=True, verbose_name='قیمت در روز')
+    is_available = models.BooleanField(default=True, verbose_name='فعال')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'وسیله نقلیه'
+        verbose_name_plural = 'وسایل نقلیه'
+
+    def __str__(self):
+        return self.name
 
 #Images for Vehicles
 class VehicleImages(models.Model):
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='vehicle_images/')
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='images', verbose_name='وسیله')
+    image = models.ImageField(upload_to='vehicle_images/', verbose_name='عکس')
 
     class Meta:
         db_table = 'vehicles_vehicle_images'
+        verbose_name = 'عکس وسیله نقلیه'
+        verbose_name_plural = 'عکس های وسایل نقلیه'
 
     def __str__(self):
         image_number = 0
