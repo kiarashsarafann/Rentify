d4f38d77af541b5b00ae14aaff90f19c92dbdd49
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2026-02-12 01:01:20 +0330

    change model for payments table

--- RentifyBackend/payment/models.py
+++ RentifyBackend/payment/models.py
@@ -10,10 +10,17 @@ class PaymentStatus(models.TextChoices):
     FAILED = "failed", "Failed"
 
 class Payment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=12, decimal_places=0)
-    authority = models.CharField(max_length=255, blank=True, null=True)
-    ref_id = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(max_length=10, choices=PaymentStatus.choices, default=PaymentStatus.PENDING)
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    reservation = models.OneToOneField(Reservation, on_delete=models.CASCADE, verbose_name='رزرو')
+    amount = models.DecimalField(max_digits=12, decimal_places=0, verbose_name='قیمت کل')
+    authority = models.CharField(max_length=255, blank=True, null=True, verbose_name='کد هویت')
+    ref_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='کد رهگیری')
+    status = models.CharField(max_length=10, choices=PaymentStatus.choices, default=PaymentStatus.PENDING, verbose_name='وضعیت')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'پرداخت'
+        verbose_name_plural = 'پرداخت ها'
+
+    def __str__(self):
+        return f"{self.user} | {self.ref_id}"
