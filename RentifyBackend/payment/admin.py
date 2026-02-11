8fe7daaceeab5429bcfc12c6e9c71ad9fd0e017e
kiarash_sarafan <kiarash007.sarafan@gmail.com>
2026-02-12 01:01:03 +0330

    make admin for payment

--- RentifyBackend/payment/admin.py
+++ RentifyBackend/payment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reservation', 'amount', 'authority', 'ref_id', 'status', 'created_at')
